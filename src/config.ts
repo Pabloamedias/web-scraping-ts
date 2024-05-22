84b2936fc61c8c38fa839a9094cfcc3a17943f5e
Pabloamedias <p.olmedosaez@gmail.com>
2024-05-22 16:47:46 -0400

    change BASEURL to BASE_URL, set the topic as an enviroment variable

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export default   {
-    BASEURL : process.env.BASEURL || 'https://chileservicios.com',
+    BASE_URL : process.env.BASE_URL || 'https://chileservicios.com',
+    TOPIC_TO_SCRAP : process.env.TOPIC_TO_SCRAP || '/videojuegos/',
     OUTPUT_DATA_DIRECTORY : process.env.OUTPUT_DATA_DIRECTORY || './outputData'
 }
